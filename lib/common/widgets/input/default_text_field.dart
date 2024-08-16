792aa2c8fdad06e6b4f0ff842d1842cd66bb4be5
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-16 23:35:11 +0600

    default text field updated

--- lib/common/widgets/input/default_text_field.dart
+++ lib/common/widgets/input/default_text_field.dart
@@ -1,16 +1,23 @@
 import 'package:authentication_with_bloc_firebase_flutter/utils/constants/app_color.dart';
 import 'package:flutter/material.dart';
 
-
-class DefaultTextField extends StatelessWidget {
+class DefaultTextField extends StatefulWidget {
   final String hintText;
   final TextEditingController controller;
+  final bool isPassword;
   const DefaultTextField({
     super.key,
     required this.hintText,
     required this.controller,
+    this.isPassword = false,
   });
 
+  @override
+  State<DefaultTextField> createState() => _DefaultTextFieldState();
+}
+
+class _DefaultTextFieldState extends State<DefaultTextField> {
+  bool visible = false;
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
@@ -18,8 +25,29 @@ class DefaultTextField extends StatelessWidget {
         maxWidth: 400,
       ),
       child: TextFormField(
-        controller: controller,
+        controller: widget.controller,
+        obscureText: widget.isPassword ? !visible : visible,
+        autocorrect: !widget.isPassword,
+        enableSuggestions: !widget.isPassword,
         decoration: InputDecoration(
+          suffixIcon: widget.isPassword
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: IconButton(
+                    style: const ButtonStyle(
+                        splashFactory: NoSplash.splashFactory),
+                    onPressed: () {
+                      setState(() {
+                        visible = !visible;
+                      });
+                    },
+                    icon: Icon(
+                      visible ? Icons.visibility : Icons.visibility_off,
+                      color: AppColor.borderColor,
+                    ),
+                  ),
+                )
+              : null,
           contentPadding: const EdgeInsets.all(27),
           enabledBorder: OutlineInputBorder(
             borderSide: const BorderSide(
@@ -35,7 +63,7 @@ class DefaultTextField extends StatelessWidget {
             ),
             borderRadius: BorderRadius.circular(10),
           ),
-          hintText: hintText,
+          hintText: widget.hintText,
         ),
       ),
     );
