77bebd77efb374938aa4e4c75bc5eabd74bc2f83
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-17 20:25:19 +0600

    feat: Update Home UI with logout functionality and error handling
    Refactor the Home UI to include a logout button and handle logout functionality using the HomeBloc. Also, improve error handling in the SignupBloc.

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,26 +1,52 @@
+import 'package:authentication_with_bloc_firebase_flutter/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/home_bloc.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        // Add your code here
-
-        child: const Icon(Icons.add),
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      listener: (context, state) {
+        if (state is HomeLogoutSuccess) {
+          Navigator.popUntil(context, (route) => route.isFirst);
+        } else if (state is HomeLogoutFailure) {
+          HelperFunction.showSnackBar(context, state.error);
+        }
+      },
+      builder: (context, state) {
+        if (state is HomeLogoutInProgress) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text('Home'),
+            actions: [
+              //logout button
+              IconButton(
+                icon: const Icon(Icons.exit_to_app),
+                onPressed: () {
+                  context.read<HomeBloc>().add(LogoutButtonPressed());
+                },
+              ),
+            ],
+          ),
+          body: const Center(
+            child: Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        );
+      },
     );
   }
 }
